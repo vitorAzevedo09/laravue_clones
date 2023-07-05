f2ee11a996506db1b08d61774aedf9bf7355c0b1
Vitor Silva <vitor.azevedo009@gmail.com>
2023-07-05 13:06:44 -0300

    Building MenuItem

---
+++ spotify-clone/src/components/MenuItem.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+import { ref, toRefs, watchEffect } from 'vue';
+import useRouter from 'vue-router';
+
+const route = useRouter()
+
+const props = defineProps({
+    iconString: String,
+    iconSize: Number,
+    pageUrl: String,
+    name: String,
+});
+
+const { iconString, pageUrl, name, iconSize } = toRefs(props);
+
+let icon = ref(null)
+
+let textIsHover = ref(false)
+    
+    
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
